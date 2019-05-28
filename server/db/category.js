bfa409fe0d66aee5ef4f65359b9692b37d95f09b
Sergio Garcia <sergio.garcia.seo@gmail.com>
2019-05-28 02:12:21 +0200

    finish category api

--- server/db/category.js
+++ server/db/category.js
@@ -3,7 +3,6 @@ const { Mysql } = require('./config');
 exports.CategoryModel = new (class CategoryModel extends Mysql {
   getCategoryById(id) {
     if (!id || isNaN(id)) return Promise.reject('Category id is not defined or is incorrect');
-
     return this.query(`SELECT * FROM Categories WHERE id_category=${id}`);
   }
 
@@ -11,24 +10,19 @@ exports.CategoryModel = new (class CategoryModel extends Mysql {
     return this.query('SELECT * FROM Categories');
   }
 
-  // getCategoriesByRol(rolId) {
-  //   return this.query(`SELECT * FROM Categories WHERE rol=${rolId}`);
-  // }
-
-  createCategory({ name, excerpt, meta_title, meta_description, date_add }) {
-    if (!name || !excerpt || !meta_title || !meta_description || !date_add)
+  createCategory({ name, excerpt, meta_title, meta_description }) {
+    if (!name || !excerpt || !meta_title || !meta_description)
       return Promise.reject('Category data is incomplete');
+    const date = new Date().toISOString();
 
     return this.query(
-      `INSERT INTO Categories 
-      (name, excerpt, meta_title, meta_description, date_add) 
-      VALUES (${name},${excerpt},${meta_title},${meta_description},${date_add})`,
+      'INSERT INTO Categories (name, excerpt, meta_title, meta_description, date_add) ' +
+        `VALUES ('${name}','${excerpt}','${meta_title}','${meta_description}','${date}')`,
     );
   }
 
   deleteCategory(id) {
     if (!id || isNaN(id)) return Promise.reject('Category id is not defined or is incorrect');
-
     return this.query(`DELETE FROM Categories WHERE id_category=${id}`);
   }
 
@@ -38,9 +32,9 @@ exports.CategoryModel = new (class CategoryModel extends Mysql {
     if (!id || isNaN(id)) return Promise.reject('Category id is not defined or is incorrect');
 
     return this.query(
-      `UPDATE Categories
-      SET name=${name},excerpt=${excerpt},meta_title=${meta_title},meta_description=${meta_description} 
-      WHERE id_category=${id}`,
+      `UPDATE Categories` +
+        ` SET name='${name}',excerpt='${excerpt}',meta_title='${meta_title}',meta_description='${meta_description}'` +
+        ` WHERE id_category=${id}`,
     );
   }
 })();
