6842caee425c263d2691e6bfb0783a82513dac0b
xabertum <mohatar@gmail.com>
2019-06-08 11:09:19 +0200

    minor css changes

--- components/admin-list-product/index.js
+++ components/admin-list-product/index.js
@@ -7,6 +7,8 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Create from '@material-ui/icons/Create';
 
 import './admin-list-product.scss';
 
@@ -282,14 +284,6 @@ export class ListProduct extends PureComponent {
 
     console.log(this.props.data);
 
-    function HomeIcon(props) {
-      return (
-        <SvgIcon {...props}>
-          <path fill="#FFFFFF" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
-        </SvgIcon>
-      );
-    }
-
     return (
       <div>
         <Typography variant="h6">
@@ -309,9 +303,14 @@ export class ListProduct extends PureComponent {
 
                 }
                 actionIcon={
-                  <IconButton>
-                    <HomeIcon />
-                  </IconButton>
+                  <div className="display-flex">
+                    <IconButton>
+                      <Create className="white-color"></Create>
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon className="white-color"></DeleteIcon>
+                    </IconButton>
+                  </div>
                 }
               />
             </GridListTile>
