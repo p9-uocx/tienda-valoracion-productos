0a8717b6c35b27961456b3db12fa9e1fc53dd6db
xabertum <mohatar@gmail.com>
2019-06-08 09:01:02 +0200

    user list work

--- components/admin-list-user/index.js
+++ components/admin-list-user/index.js
@@ -2,14 +2,12 @@ import React, { PureComponent } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AccountCircleTwoTone from '@material-ui/icons/AccountCircleTwoTone'
 import Create from '@material-ui/icons/Create';
@@ -62,22 +60,25 @@ export class ListUser extends PureComponent {
                 </ListItemAvatar>
                 <ListItemText
                   primary={
-                    <div className="info-block-elem user-list">
-                      <span >First Name: {elem.first_name}</span>
-                      <span>Last Name: {elem.last_name}</span>
-                      <span>Email: {elem.email}</span>
-                      <span>Date Added: {elem.date_add}</span>
-                      <span>Rol: {elem.rol}</span>
+                    <div>
+                      <div className="user-list">
+                        <span>First Name: <b>{elem.first_name}</b></span>
+                        <span> - Last Name: <b>{elem.last_name}</b></span>
+                      </div>
+                      <div className="user-list">
+                        <span>Email: <b>{elem.email}</b></span>
+                        <span> - Date Added: <b>{elem.date_add}</b></span>
+                        <span> - Rol: <b>{elem.rol}</b></span>
+                      </div>
                     </div>
                   }
-
                 />
                 <ListItemSecondaryAction>
                   <IconButton edge="end" aria-label="Delete">
-                    <Create></Create>                    
+                    <Create></Create>
                   </IconButton>
                   <IconButton edge="end" aria-label="Delete">
-                    <DeleteIcon></DeleteIcon>                    
+                    <DeleteIcon></DeleteIcon>
                   </IconButton>
                 </ListItemSecondaryAction>
               </ListItem>
@@ -85,8 +86,6 @@ export class ListUser extends PureComponent {
           </List>
         </div>
       </Grid>
-
-
     </div>;
   }
 }
